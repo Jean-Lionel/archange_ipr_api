dd62e3dbb67eb393e1662a33433f81761bb16c2c
Jean-Lionel <nijeanlionel@gmail.com>
2023-08-10 18:26:04 +0200

    format routers

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdresseController;
+use App\Http\Controllers\ContribuableController;
 use App\Http\Controllers\PaimentIprController;
+use App\Http\Controllers\TauxImposambleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,15 +21,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('adresse', App\Http\Controllers\AdresseController::class);
 
-Route::apiResource('contribuable', App\Http\Controllers\ContribuableController::class);
+Route::post('register', [UserController::class, 'register']);
 
+Route::post('login',[UserController::class, 'login'] );
 
-Route::apiResource('employe', App\Http\Controllers\EmployeController::class);
+Route::middleware('auth:sanctum')->group(function () {
 
+    Route::apiResource('adresse', AdresseController::class);
 
-Route::apiResource('paiment-ipr', PaimentIprController::class);
+    Route::apiResource('contribuable', ContribuableController::class);
+    Route::apiResource('employe', EmployeController::class);
 
-Route::get('generate_ipr', [PaimentIprController::class, 'generate_ipr']);
-Route::apiResource('taux-imposamble', App\Http\Controllers\TauxImposambleController::class);
+    Route::apiResource('paiment-ipr', PaimentIprController::class);
+    Route::get('generate_ipr', [PaimentIprController::class, 'generate_ipr']);
+    Route::apiResource('taux-imposamble', TauxImposambleController::class);
+
+});
